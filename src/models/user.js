21928d67f4f60b87f33641879aae8f17b37a2d28
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-03-26 20:04:30 +0530

    added user schema file

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require('crypto');
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -13,9 +14,23 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
+  password:{
+    type:String,
+    required:true,
+    max:1024,
+    min:5
+  }
+  // hash:String,
+  // salt : String
 });
+// UserSchema.methods.setPassword = function(password) {
+// this.salt = crypto.randomBytes(16).toString('hex');
+// this.hash = crypto.pbkdf2Sync(password, this.salt, 
+//   1000, 20 ).toString(`hex`);
+// }
+//   UserSchema.methods.validPassword = function(password) {
+//     const hash = crypto.pbkdf2Sync(password, 
+//     this.salt, 1000, 20).toString(`hex`);
+//     return this.hash === hash;
+// };
 module.exports = mongoose.model("User", UserSchema);
